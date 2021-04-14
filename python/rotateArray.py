1997b71b785b0b6a27e417a4c6c673a9fc593788
Christian Herrera <christian@christian-herrera.com>
2021-04-13 22:30:53 -0700

    Updated code for rotateLeft()

--- python/rotateArray.py
+++ python/rotateArray.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def rotateArray(d, arr):
+def rotateRight(d, arr):
     tmp_arr = arr.copy()
     count = 1
     rotate = True
@@ -14,5 +14,21 @@ def rotateArray(d, arr):
     return tmp_arr
 
 
+def rotateLeft(d, arr):
+    tmp_arr = arr.copy()
+    count = 1
+    rotate = True
+    while rotate:
+        if count == d:
+            rotate = False
+        tmp = tmp_arr.pop(0)
+        last = len(tmp_arr)
+        tmp_arr.insert(last, tmp)
+        count += 1
+
+    return tmp_arr
+
+
 arr1 = [1, 2, 3, 4, 5]
-print(rotateArray(3, arr1))
+print(rotateRight(3, arr1))
+print(rotateLeft(4, arr1))
